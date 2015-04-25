ac2c2f6ed47adc8ab2c3bce5395e721703100178
velhonoja <jvelhonoja@gmail.com>
2015-04-25 10:17:29 +0300

    install file

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -2,6 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
+
+class Install extends CI_Controller {
+
+	public function install() {
+		// do stuff here, or something...
+		
+		
+		
+		$this->load->view('welcome_message');
+	}
+}
+
+
+
 /* 
 	CREATE TABLE `USER` (
 	`USER_ID` int(11) NOT NULL auto_increment,

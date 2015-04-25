ec21f9aad3e7b53f56953d1e3dfc000e70aa10c0
Reima Karvonen <reima.karvonen@gmail.com>
2015-04-25 09:35:35 +0300

    Default welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,8 +18,7 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index() {
+		$this->load->view('welcome_message', $data);
 	}
 }

b7cf4392fd5aacbcaf88de64a03f32b53acb85c0
velhonoja <jvelhonoja@gmail.com>
2015-04-25 11:17:29 +0300

    foo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,6 +19,20 @@ class Welcome extends CI_Controller {
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index() {
-		$this->load->view('welcome_message');
+		
+
+$query = $this->db->query("CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_firstname` varchar(100) NOT NULL,
+  `user_lastname` varchar(100) NOT NULL,
+  `user_disabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;");
+
+			$this->load->view('welcome_message');
+		
+		
+		
+		
 	}
 }

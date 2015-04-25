4a9d43c6525ebb4c2a5507428dcd297ef34adfde
Reima Karvonen <reima.karvonen@gmail.com>
2015-04-25 12:11:25 +0300

    ETM

--- application/controllers/EventTypeManager.php
+++ application/controllers/EventTypeManager.php
@@ -4,6 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class EventTypeManager extends CI_Controller {
     
-    
+    public function index() {
+        $data = array(
+            'content' => "EventTypeManager/front",
+            'foo' => "asdf"
+        );
+        
+        $this->load->view("template", $data);
+        
+    }
     
 }

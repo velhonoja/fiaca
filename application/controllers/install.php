422e71e800baf98d37e6ba9b7e6bea8f50e8a79a
velhonoja <jvelhonoja@gmail.com>
2015-04-25 09:54:01 +0300

    test

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-/*
+/* 
 	CREATE TABLE `USER` (
 	`USER_ID` int(11) NOT NULL auto_increment,
 	`USER_FIRSTNAME` VARCHAR(255) NOT NULL,

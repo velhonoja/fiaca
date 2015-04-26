3e9fd5fc95d5355682ef2ce0043b2d9d798608fe
reimakarvonen <reima.karvonen@gmail.com>
2015-04-26 12:04:12 +0300

    Created controller to handle old database transclusion actions

---
+++ application/controllers/OldBaseTransclusionManager.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class OldBaseTransclusionManager extends CI_Controller {
+    
+    public function index() {
+        echo "<h3>Old database transclusion tools</h3>";
+        
+    }
+    
+}

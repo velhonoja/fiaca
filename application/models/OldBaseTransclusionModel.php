feafb619cf4e0311ff128d8949050ef695fabd96
Reima Karvonen <reima.karvonen@gmail.com>
2015-04-26 22:32:14 +0300

    Created OldBaseTransclusionModel for handling the database actions in transclusion process.

---
+++ application/models/OldBaseTransclusionModel.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * This model handles the database queries related to old database transclusion 
+ * integration procedures.
+ * 
+ * In future solutions this can be ignored and deleted after the new system
+ * is up and running.
+ */
+

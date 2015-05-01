beb5dadbe0eb32886c020d7e706cc4259a03f044
velhonoja <jvelhonoja@gmail.com>
2015-05-01 14:45:53 +0300

    Linking to jQuery and Bootstrap.

--- application/views/header.php
+++ application/views/header.php
@@ -2,8 +2,19 @@
 <html>
 
     <head>
+    	<?php echo meta('Content-type', 'text/html; charset=utf-8', 'equiv'); ?>
+    	
+    	 <!-- jQuery -->
+    	 <script type='text/javascript' src="<?php echo base_url(); ?>vendor/jquery/dist/jquery.js"></script>
+    	 
+    	<!-- Twitter Bootstrap -->
+    	<?php echo link_tag('vendor/bootstrap/dist/css/bootstrap.css'); ?>
+   	<script type='text/javascript' src="<?php echo base_url(); ?>vendor/bootstrap/dist/js/bootstrap.js"></script>
+
+    	 
+    	 
         <?php echo link_tag('css/default.css'); ?>
-        <?php echo meta('Content-type', 'text/html; charset=utf-8', 'equiv'); ?>
+        
         <title>Fiaca</title>
     </head>
     <body>

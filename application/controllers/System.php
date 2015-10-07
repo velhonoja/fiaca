f699cfb0a27a2f58a4c553ec9065586e21bc8e3f
Reima Karvonen <reima.karvonen@gmail.com>
2015-10-07 11:21:21 +0300

    Created system controller for useless use

---
+++ application/controllers/System.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * This can be The Main controller of the System
+ * Replaces the Welcome later.
+ */
+
+class System extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    
+    
+    
+}

60d3fe098a1120d58ec8eaf25aa385b74799659f
Reima Karvonen <reima.karvonen@gmail.com>
2015-10-07 11:49:17 +0300

    Added event manager link and mobile

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -7,9 +7,28 @@ http://getbootstrap.com/components/#navbar
 
 -->
 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#foo" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <!-- <a class="navbar-brand" href="#">Brand</a> -->
+    </div>
+
+
+<div class="collapse navbar-collapse" id="foo">
+
 <ul class="nav nav-tabs">
 		<li>
 			<?php echo anchor("welcome", "Dashboard"); ?>
+		</li>
+		<li>
+			<?php echo anchor("eventManager", "Manage events"); ?>
 		</li>
 		<li>
 			<?php echo anchor("eventTypeManager", "Manage event types"); ?>
@@ -56,3 +75,8 @@ http://getbootstrap.com/components/#navbar
 		
 		
 </ul>
+    
+    
+    </div>
+
+</nav>

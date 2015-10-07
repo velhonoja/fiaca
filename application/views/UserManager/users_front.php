854140a81430b457bbfe5f91533e985e42caba89
Reima Karvonen <reima.karvonen@gmail.com>
2015-10-07 11:00:05 +0300

    Created bs layout

--- application/views/UserManager/users_front.php
+++ application/views/UserManager/users_front.php
@@ -1,29 +1,37 @@
 <h1>User Manager</h1>
 
-<h3>Users</h3>
+<div class="panel panel-primary">
 
-<table>
-        <tr style="font-weight: bold;">
-            <td><p>Sukunimi</p></td>
-            <td><p>Etunimi</p></td>
-            <td><p>User ID</p></td>
-            <td><p>Toiminnot</p></td>
-        </tr>
-    
-        <?php 
-        foreach($users as $user) {
-            
-            echo "<tr>
+    <div class="panel-heading">
+
+        <h3 class="panel-title">Users</h3>
+
+    </div>
+
+    <div class="panel-body">
+
+        <table>
+            <tr style="font-weight: bold;">
+                <td><p>Sukunimi</p></td>
+                <td><p>Etunimi</p></td>
+                <td><p>User ID</p></td>
+                <td><p>Toiminnot</p></td>
+            </tr>
+
+            <?php
+            foreach ($users as $user) {
+
+                echo "<tr>
             <td><p>" . $user->user_lastname . "</p></td>
             <td><p>" . $user->user_firstname . "</p></td>
             <td><p>" . $user->user_id . "</p></td>
             <td><p>" . anchor("UserManager/edit/" . $user->user_id, "Edit") . "</p></td>
                 </tr>";
-            
-            
-        }
-            
-        ?>
-        
-        
-</table>
+            }
+            ?>
+
+
+        </table>
+
+    </div>
+</div>
